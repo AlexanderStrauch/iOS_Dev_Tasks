9c5dbc804a0975e70f4ae64ff706cb3ebbef89a3
Alexander Strauch <alexstrauch98[|]]{]|}|{[>
2022-06-18 15:34:41 +0200

    Initial Commit

---
+++ iOS_Dev_Tasks/iOS_Dev_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Dev_TasksApp.swift
+//  iOS_Dev_Tasks
+//
+//  Created by Alexander Strauch on 18.06.22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Dev_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
